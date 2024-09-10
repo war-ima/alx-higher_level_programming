33f2a2d2ee07426fa51dd3b0043e2a62e7a28dd7
Natasha <MY_NAME@example.com>
2024-09-10 13:10:39 -0700

    4C commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for small_letter in range(97, 123):
-    if chr(small_letter)!= 'q' and chr(small_letter) != 'e':
+    if chr(small_letter) != 'q' and chr(small_letter) != 'e':
         print("{}".format(chr(small_letter)), end="")
